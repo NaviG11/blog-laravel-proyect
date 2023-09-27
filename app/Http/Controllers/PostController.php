f608e37137497678448413d3601e64cbb2c2cca5
IVAN CARLOS QUISPE PATTY <ivancarlosqp@gmail.com>
2023-09-27 02:43:44 -0400

    feat: :construction: implementación de la caché

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,12 +6,19 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Tag;
+use Illuminate\Support\Facades\Cache;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::where('status', 2)->latest('id')->paginate(8);
+        if (Cache::has('posts')) {
+            $posts = Cache::get('posts');
+        } else {
+            $posts = Post::where('status', 2)->latest('id')->paginate(8);
+            Cache::put('posts', $posts);
+        }
+        // $posts = Post::where('status', 2)->latest('id')->paginate(8);
         return view('posts.index', compact('posts'));
     }
     public function show(Post $post)
