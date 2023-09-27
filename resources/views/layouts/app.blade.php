1e27faa73f1b51a0789b15d4105f89984b17f694
Carla Choque <carlachoquelimachi@gmail.com>
2023-09-27 11:36:24 -0400

    respando antes de poner el banner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,18 +21,12 @@
         <x-banner />
 
 
-
-
         <div class="min-h-screen mt-0 bg-left justify-center" style="background-image: url('https://img.freepik.com/vector-gratis/fondo-textura-hoja-papel-arrugado-blanco_1017-31714.jpg'); background-size: cover;">
             @livewire('navigation')
             
-            <!-- Establece la imagen como fondo -->
-            <div class="mt-8 bg-left justify-center" style="background-image: url('https://cdn.pixabay.com/photo/2023/03/28/07/51/trees-7882545_640.jpg'); background-size: cover;">
-                <!-- Contenido que deseas sobre el fondo -->
+            <div>
+                
             </div>
-
-
-
             <!-- Page Content -->
             <main>
                 {{ $slot }}
