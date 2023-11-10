f2c40b862650ab09372e72c0fd2e2a70a74b3d10
AmandaChoque <carlachoquelimachi@gmail.com>
2023-11-10 01:39:18 -0400

    livewire_ avence

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -1,4 +1,5 @@
-<div ref="www.google.com">
-    <a>hola</a>
-
+<div>
+    <button wire:click="redirectToGoogle" class="bg-blue-500 text-white px-4 py-2 rounded-lg">
+        Ir a Google
+    </button>
 </div>
